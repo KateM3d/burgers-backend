086795b7a37cf24cd76241796edf4b46233c6632
KateM3d <ekaterina8medvedeva@gmail.com>
2023-07-20 20:34:25 -0400

    first commit

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require("express");
 
 const app = express();
-const PORT = 3000 || process.env.port;
+const PORT = process.env.PORT || 3000;
+
 const cors = require("cors");
+const burgersData = require("./data");
 
 require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
+
+app.get("/burgers", (req, res) => {
+  res.json(burgersData);
+});
 
 app.listen(PORT, () => {
   console.log("Server is listening on port " + PORT);
